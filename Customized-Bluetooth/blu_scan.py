680cd4646b01218ce24443f2253dd53e310dd28c
Esther <46161506+Esther-Ye@users.noreply.github.com>
2021-09-15 12:38:22 -0400

    Create blu_scan.py

---
+++ Customized-Bluetooth/blu_scan.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Example of scanning Bluetooth Low Energy (BLE) devices.
+Requires a Linux computer due to gattlib underlying BLE scanning requiring Glib.
+"""
+
+
+
+import argparse
+from bluetooth.ble import DiscoveryService
+
+p = argparse.ArgumentParser(description="BLE scanner")
+p.add_argument(
+    "timeout",
+    help="number of seconds to scan for BLE devices",
+    nargs="?",
+    type=int,
+    default=5,
+)
+P = p.parse_args()
+
+timeout = P.timeout
+
+print(f"Scanning BLE devices for {timeout} seconds")
+
+svc = DiscoveryService()
+ble_devs = svc.discover(timeout)
+
+# 09/15/21 formatting print output
+# for u, n in ble_devs.items():
+#    print(u, n)
+
+for address, name in ble_devs.items():
+    print("name: {}, address: {}".format(name, address))
