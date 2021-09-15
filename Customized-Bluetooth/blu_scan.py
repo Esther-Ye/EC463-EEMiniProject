3f79715e67bcf56d5f4262a4ddab3ec45b749c9f
Ye <estherye@bu.edu>
2021-09-15 14:22:04 -0400

    fixed timestamp

--- Customized-Bluetooth/blu_scan.py
+++ Customized-Bluetooth/blu_scan.py
@@ -18,20 +18,19 @@ p.add_argument(
     help="number of seconds to scan for BLE devices",
     nargs="?",
     type=int,
-    default=30,
+    default=5,
 )
 P = p.parse_args()
 
 timeout = P.timeout
 
-print(f"Scanning BLE devices for {timeout} seconds")
-
-svc = DiscoveryService()
-ble_devs = svc.discover(timeout)
+for x in range(5):
+    print(f"Scanning BLE devices for {timeout} seconds")
+    svc = DiscoveryService()
+    ble_devs = svc.discover(timeout)
 
 # 09/15/21 formatting print output
-# for u, n in ble_devs.items():
-#    print(u, n)
+    for u, n in ble_devs.items():
+        print(u, n, datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
 
-for address, name in ble_devs.items():
-    print("address: {}, time: {}".format(address, datetime.now()))
+
